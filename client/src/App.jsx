6c407828838dbcf3d02cd7a199295eadd1444d5e
Sam-Cowman <sam.p.cowman@gmail.com>
2024-07-24 17:30:05 -0500

    added logic for apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
+// client/src/App.jsx
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import ApolloProviderWrapper from './apolloClient.jsx';
 
 function App() {
   return (
-    <>
+    <ApolloProviderWrapper>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProviderWrapper>
   );
 }
 
